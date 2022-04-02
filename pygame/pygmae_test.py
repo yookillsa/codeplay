0e276ada477984760b6295e112df1b25097d22d3
yookillsa <codepaly.yoonsoo@gamil.com>
2022-04-02 14:44:28 +0900

    파이게임 수정

--- pygame/pygmae_test.py
+++ pygame/pygmae_test.py
@@ -9,7 +9,7 @@ clock = pygame.time.Clock()
 while True: 
     screen.fill((0,0,0))
 
-    event = pygame.event.poil()
+    event = pygame.event.poll()
     if event.type == pygame.QUIT:
         break
 
